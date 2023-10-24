2ed458c7082f57432523a1d3bd18041dd67548d5
Quezia <quezia.kato@alunoescolassociais.org.br>
2023-10-24 08:23:24 -0300

    validação

---
+++ cadastro.js
@@ -0,0 +1,26 @@
+//#################
+
+//Código de validação de formulário de Cadastro E-commerce
+
+function verificar() {
+    let Nome = document.getElementById('nome').value;
+    let DatadeNascimento = document.getElementById('dtnasc').value;
+    let CPF = document.getElementById('CPF').value;
+    let Email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let CEP = document.getElementById('CEP').value;
+    let Endereço = document.getElementById('Endereco').value;
+    let Numero = document.getElementById('Numero').value;
+    let Complemento = document.getElementById('Complemento').value;
+    let Bairro = document.getElementById('Bairro').value;
+    let Estado = document.getElementById('Estado').value;
+    let Ptr = document.getElementById('Ptr').value;
+    let confirmSenha = document.getElementById('confirmsenha').value;
+    let senha = document.getElementById('Senha').value; 
+    {
+    if (!Nome || !DatadeNascimento || !CPF || !Email || !telefone || !CEP || !Endereço || !Numero || !Complemento || !Bairro || !Estado || !Ptr || !confirmSenha || !senha) {
+   alert("Campo de preenchimento obrigatório");
+    }else
+    alert("Campos preenchidos com sucesso!");
+    }
+}
